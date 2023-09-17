aaec76a2ad65cf2d3f8e205675f01f49c17f16a3
Andy <andyphung64@gmail.com>
2023-09-17 07:05:13 -0400

    add register n auth

--- src/register.js
+++ src/register.js
@@ -6,129 +6,86 @@ import axios from 'axios';
 import FormData from 'form-data';
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
+import { Navigate } from "react-router-dom";
 
 export class Register extends React.Component {
-  image_url = "";
+
   constructor(props) { 
     
     super(props);
-  }
-  render() {
+    this.state = {
+      image_url: "",
+      submitted: false,
+      authenticated: false
+    }
 
-    async function previewFile() {
-      const preview = document.querySelector("img");
-      const file = document.querySelector("input[type=file]").files[0];
-      console.log(file);
-      const reader = new FileReader();
+  }
+
   
 
-      var base = "bleh";
-
-      if (file) {
-        base = String(reader.readAsDataURL(file));
-      }
 
 
-      reader.onload = async function(event) {
-        base = event.target.result;
-        base = base.split(",").pop();
-        console.log(base);
-
-
-        /*
-
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", "https://api.imgbb.com/1/upload?key=f152283cb0e579eb5538807d81190463");
-        //xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-
-        const body = JSON.stringify({
-          image: base
-        });
-
-        xhr.onload = () => {
-          if (xhr.readyState == 4 && xhr.status == 201) {
-            console.log(JSON.parse(xhr.responseText));
-          } else {
-            console.log(`Error: ${xhr.status}`);
-          }
-        };
-        xhr.send(body);
-
-        };
-
-        */
+    //reader.onload = async function(event) {
     
-        let body = new FormData();
-        body.set('key', 'f152283cb0e579eb5538807d81190463');
-        body.append('image', base);
+  //}
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    // event.target.elements.username.value
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyCPXQnlJg7oVQNhCEBnKHrE7syPLWRnm7w",
+      authDomain: "firestorm-f1b5d.firebaseapp.com",
+      projectId: "firestorm-f1b5d",
+      storageBucket: "firestorm-f1b5d.appspot.com",
+      messagingSenderId: "916205727733",
+      appId: "1:916205727733:web:4703a3988e5c311b607abc",
+      measurementId: "G-KMW0CSZS93"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+    // Initialize Cloud Firestore and get a reference to the service
+    const db = firebase.firestore();
+
+    console.log(this.image_url);
+
+    db.collection("user-db").add({
+      firstname: event.target.elements.firstname.value,
+      lastname: event.target.elements.lastname.value,
+      kerb: event.target.elements.kerb.value,
+      year: event.target.elements.year.value,
+      dorm: event.target.elements.dorm.value,
+      gpa: event.target.elements.gpa.value,
+      classes: event.target.elements.classes.value,
+      imagelink: this.state.image_url,
+      pw: event.target.elements.pw.value,
+      bio: event.target.elements.bio.value
+    })
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+
+    this.setState({submitted: true});
+    this.setState({authenticated: true});
+
+    console.log("form submitted");
+
     
-        let response = await axios({
-          method: 'post',
-          url: 'https://api.imgbb.com/1/upload',
-          data: body
-        });
+    // firebase shit goes here
+  };
 
-        const url = response.data.data.url;
-
-        this.image_url = url;
-
-      
-      
-    }
-
-  }
-
-
-    const handleSubmit = (event) => {
-      event.preventDefault();
-
-      // event.target.elements.username.value
-
-      const firebaseConfig = {
-        apiKey: "AIzaSyCPXQnlJg7oVQNhCEBnKHrE7syPLWRnm7w",
-        authDomain: "firestorm-f1b5d.firebaseapp.com",
-        projectId: "firestorm-f1b5d",
-        storageBucket: "firestorm-f1b5d.appspot.com",
-        messagingSenderId: "916205727733",
-        appId: "1:916205727733:web:4703a3988e5c311b607abc",
-        measurementId: "G-KMW0CSZS93"
-      };
-
-      firebase.initializeApp(firebaseConfig);
-      // Initialize Cloud Firestore and get a reference to the service
-      const db = firebase.firestore();
-
-      db.collection("user-db").add({
-        firstname: event.target.elements.firstname.value,
-        lastname: event.target.elements.lastname.value,
-        kerb: event.target.elements.kerb.value,
-        year: event.target.elements.year.value,
-        dorm: event.target.elements.dorm.value,
-        gpa: event.target.elements.gpa.value,
-        classes: event.target.elements.classes.value,
-        imagelink: this.image_url,
-        pw: event.target.elements.pw.value,
-        bio: event.target.elements.bio.value
-      })
-      .then((docRef) => {
-          console.log("Document written with ID: ", docRef.id);
-      })
-      .catch((error) => {
-          console.error("Error adding document: ", error);
-      });
-
-      
-      // firebase shit goes here
-    }
-
-
+  render() {
     return (
       <div id="register_page">
         <div id="header">
           <img id="Home_logo" src={logo} alt = "Firestorm Logo" />
           <h1 id="Firestorm_name">Firestorm</h1>
         </div>
-        <form id="register_form" onSubmit={handleSubmit}> 
+        <form id="register_form" onSubmit={this.handleSubmit}> 
         <span>First name:</span><br/>
         <input type="text" name="firstname" /><br/>
         <span>Last name:</span><br/>
@@ -162,7 +119,43 @@ export class Register extends React.Component {
         <span>Biography:</span><br/>
         <input type="text" name="bio"/><br/>
         <span>Upload a profile picture:</span><br/>
-        <input type="file" name="image" onInput={previewFile}/><br/>
+        <input type="file" name="image" onInput={async () => {
+    const preview = document.querySelector("img");
+    const file = document.querySelector("input[type=file]").files[0];
+    console.log(file);
+    const reader = new FileReader();
+
+
+    var base = "bleh";
+
+    if (file) {
+      base = String(reader.readAsDataURL(file));
+    }
+
+    reader.onload = async (event) => {
+      base = event.target.result;
+      base = base.split(",").pop();
+      console.log(base);
+
+      let body = new FormData();
+      body.set('key', 'f152283cb0e579eb5538807d81190463');
+      body.append('image', base);
+
+      let response = await axios({
+        method: 'post',
+        url: 'https://api.imgbb.com/1/upload',
+        data: body
+      });
+
+      const url = response.data.data.url;
+
+      //this.image_url = url;
+      this.setState({image_url: url});
+
+    };
+
+    this.setState({image_url: reader.image_url});
+  }}/><br/>
         <span>Password:</span><br/>
         <input type="text" name="pw"/><br/>
         <div id="submit_container">
@@ -170,6 +163,10 @@ export class Register extends React.Component {
         </div>
         
         </form>
+        {this.state.submitted && (
+          <Navigate to="/main" state={this.state} replace={true} />
+        )}
+
       </div>
     );
   }
