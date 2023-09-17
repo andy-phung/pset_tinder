a9151747f838b3e5458ffbc5d6c15144bba8481c
Matthew Chen <mattgchin777@gmail.com>
2023-09-17 03:28:54 -0400

    implemented mainpage

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import logo from './firestorm_logo.png';
+import left from './swipe_left.png';
+import right from './swipe_right.png';
 import './index.css';
 
 export class Main extends React.Component {
@@ -8,21 +10,36 @@ export class Main extends React.Component {
   }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div class = "swipe_bg">
+        <header id="swipe_logo">
+          <h1>Firestorm</h1>
         </header>
+        <div>
+          <center>
+            <button type="button" id="swipe_left">
+              <img src={left} alt="swipe left" width="120" height="auto"/>
+            </button>
+            {/* Profil/bio to be added goes here */}
+            <div id="profile_box">
+              <div class="bio">
+                <img src="" alt="profile picture"/>
+                <p><b>First Name:</b></p>
+                <p><b>Last Name:</b></p>
+                <p><b>Year:</b></p>
+                <p><b>Dorm:</b></p>
+                <p><b>GPA:</b></p>
+                <p><b>Classes:</b></p>
+                <p><b>Bio:</b></p>
+              </div>
+            </div>
+            <button type="button" id="swipe_right">
+              <img src={right} alt="swipe right" width="120" height="auto"/>
+            </button>
+          </center>
+        </div>
+        <div class="swipe_bg"> 
+          <br></br><br></br><br></br>
+        </div>
       </div>
     );
   }
